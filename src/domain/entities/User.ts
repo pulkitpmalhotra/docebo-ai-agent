870cfc947c54f1433c83161f737b67685f616756
pulkitpmalhotra <78749444+pulkitpmalhotra@users.noreply.github.com>
2025-08-30 09:28:47 -0400

    Create User.ts

---
+++ src/domain/entities/User.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  status: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
