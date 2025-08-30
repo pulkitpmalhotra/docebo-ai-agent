ec7bedf29a82ba8b4c2a3b535f04a9f671a2f762
pulkitpmalhotra <78749444+pulkitpmalhotra@users.noreply.github.com>
2025-08-30 19:44:28 -0400

    Update User.ts

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -7,4 +7,14 @@ export interface User {
   status: string;
   createdAt: Date;
   updatedAt: Date;
+  role: string;
+  language: string;
+  timezone: string;
+  country: string;
+  manager: string;
+  branch: string;
+  department: string;
+  team: string;
+  profilePictureUrl: string;
+  // Add more fields as needed
 }
